f92811ed49adcd895ae3898fcd13b85a40b4476b
Dan McHenry <danpmch@gmail.com>
2021-04-25 12:51:09 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to vortex-core
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/vortex_core/core_test.clj
@@ -0,0 +1,7 @@
+(ns vortex-core.core-test
+  (:require [clojure.test :refer :all]
+            [vortex-core.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
